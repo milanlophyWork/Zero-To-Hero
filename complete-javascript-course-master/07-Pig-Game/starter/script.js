c1fb616c4ed43fc73588b7e423bd910442641b9c
milanlophyWork <lophymilan@gmail.com>
2025-09-15 15:50:36 +0530

    switching to next player

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -3,6 +3,9 @@
 // diagrams.net for designing flowcharts
 
 // selecting elements
+const player0El = document.querySelector('.player--0')
+const player1El  =document.querySelector('.player--1')
+
 const score0El = document.querySelector('#score--0') // Instead of cls use the unique id because both elements has same cls name
 const score1El = document.getElementById('score--1') // getElementById is supposed to be a little bit faster than querySelector
 const current0El = document.getElementById('current--0')
@@ -19,9 +22,9 @@ score0El.textContent = 0 // Setting the initialScore to zero
 score1El.textContent = 0
 diceEl.classList.add('hidden')
 
-
+const scores = [0, 0]
 let currentScore = 0 // Should not be inside fn if it does, then each time we roll a dice current score would be resetted
-
+let activePlayer = 0
 
 // Rolling dice functionality
 btnRoll.addEventListener('click',()=>{
@@ -36,9 +39,28 @@ btnRoll.addEventListener('click',()=>{
     if(diceValue !== 1){
         // Add diceValue to current score
         currentScore += diceValue
-        current0El.textContent = currentScore
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore
+
+        // current0El.textContent = currentScore // current score for player 0 only
     }else{
         // switch to next player
+        
+        /*if(activePlayer === 0){
+            current0El.textContent = 0
+            currentScore = 0
+            activePlayer = 1
+        } else{
+            current1El.textContent = 0
+            currentScore = 0
+            activePlayer = 0
+        } */
+
+
+        document.getElementById(`current--${activePlayer}`).textContent = 0
+        activePlayer = activePlayer === 0 ? 1 : 0
+        player0El.classList.toggle('player--active') // toggle method add a class if it is there and remove a class if it is not there.
+        player1El.classList.toggle('player--active')
+        currentScore = 0
     }
 })
 
