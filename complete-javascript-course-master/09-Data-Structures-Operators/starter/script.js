f4e9b199a74f127d1822732240e7056d335796ad
milanlophyWork <lophymilan@gmail.com>
2025-09-17 17:32:50 +0530

    Section 9 112 video rest operator

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -47,10 +47,16 @@ const restaurant = {
       close: 24,
     },
   },
+
+  orderPizza : function(mainIngredient, ...otherIng){
+    console.log(mainIngredient)
+    console.log(otherIng)
+  }
 };
 
-// Destructuring Arrays : Way of unpacking values from an array or an object into separate variables. It is to 
-// break a complex data structure down into a smaller data structure like a variable.
+    // Destructuring Arrays : Way of unpacking values from an array or an object into separate variables. It is to 
+    // break a complex data structure down into a smaller data structure like a variable.
+/*
 
 const arr = [2, 3, 4]
 const x = arr[0]
@@ -65,12 +71,12 @@ console.log(first, third);
 
 // Switching required temp variable earlier but with destructuring it made simple. // Semi colon important
 
-/*
-const temp = first;
-first = third;
-third = temp;
-console.log(first, third);
-*/
+
+// const temp = first;
+// first = third;
+// third = temp;
+// console.log(first, third);
+
 
 [first, third] = [third, first];
 console.log(first, third);
@@ -92,3 +98,66 @@ console.log(i,j,k)
 // Default Values // setting default values for variables when we are extracting them . Useful in case if we don't know the actual length of array
 const [p=1, q=1, r=1] = [8,9]
 console.log(p, q, r) // If no default values are given then r returns undefined
+*/
+
+    // Rest pattern and parameters
+
+/* Rest pattern has same syntax as spread operator (...) but it does the opposite of spread operator.
+
+    Use cases of spread operator :
+    
+      1. To build new arrays
+        const a = [1,2] , const b = [...a, 3,4]
+      2. To pass multiple values to a fn. 
+        For eg : Let  fn be function orderFd(a,,b,c){...} and dishes = ['dish1', 'dish2', 'dish3'] then fn  can be called as orderFd(...dishes)
+      
+
+    rest pattern uses the same syntax to collect multiple elements and condense them into an array. spread operator is to unpack an array while
+    rest is to pack elements into an array.
+
+    With spread we expand array and with rest we compress into array 
+
+*/
+
+// 1. Destructuring
+
+// Spread operator because ... on right side of =
+const arr = [1,2, ...[3,4]]
+console.log(arr) // 3,4 to individual elements (not inside array)
+
+// Rest operator :: We can use ... on left side of = along with destructuring
+const [a,b, ...others] = [1,2,3,4,5]
+console.log(others) // 3,4,5 into an array
+
+const [ pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ... restaurant.starterMenu] // spread operator here , rest operator in other food
+console.log(pizza, risotto, otherFood) // Destructuring first and third element of mainMenu and rest of food in starterMenu to otherFood. Other food include rest of elements not the skipped elements
+// So rest element must be the last element
+
+// Within Objects // Remaining elements will be collected into a new object not array
+
+const {sat, ...weekdays} = restaurant.openingHours
+console.log(sat, weekdays)
+
+// 2. Functions
+
+const add = function(...numbers){ // cld as Rest parameter : Takes multiple values then pack them into an array
+  console.log(numbers)
+  let sum = 0
+  for(let i=0 ; i<numbers.length; i++){ // numbers will be a array as rest operator compress values into an array 
+    sum += numbers[i]
+  }
+  console.log(sum)
+}
+
+add(2,3)
+add(5,3,7,2) // cld rest arguments
+
+const x = [23, 5, 7] // To find sum of these use spread operator . We are spreading these numbers of array 
+add(...x) // This is spread operator as we split array elements (expanded 1 array to 3 values) // Then rest operator compress 3 values to 1 array & calculate sum
+
+// rest arguments in restaurant object:
+restaurant.orderPizza('mushrooms','onion','olives', 'spinach')
+restaurant.orderPizza('mushrooms')
+
+// NOTE : spread operator is used where we would write values separated by comma
+// rest operator is used where we would write variable names separated by comma
