2fcd2b7aafcab16c038f81cb3e6b13457261eb33
milanlophyWork <lophymilan@gmail.com>
2025-09-17 15:50:01 +0530

    Section 9 Destructuring arrays

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -30,6 +30,9 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function(starterIndex, mainIndex){ // accept 2 parameters one index for starter menu and one index for main menu. 
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]] 
+  },
   openingHours: {
     thu: {
       open: 12,
@@ -45,3 +48,47 @@ const restaurant = {
     },
   },
 };
+
+// Destructuring Arrays : Way of unpacking values from an array or an object into separate variables. It is to 
+// break a complex data structure down into a smaller data structure like a variable.
+
+const arr = [2, 3, 4]
+const x = arr[0]
+const y = arr[1]
+const z = arr[2]
+
+const [a,b,c] = arr // Array destructuring instead of above method
+console.log(a, b, c)
+
+let [first, , third] = restaurant.categories; // skipping second element in category
+console.log(first, third);
+
+// Switching required temp variable earlier but with destructuring it made simple. // Semi colon important
+
+/*
+const temp = first;
+first = third;
+third = temp;
+console.log(first, third);
+*/
+
+[first, third] = [third, first];
+console.log(first, third);
+
+console.log(restaurant.order(2,0)) // return starterMenu[2] and mainMenu[0]
+
+// Receive 2 return values from a function
+const [food1, food2] = restaurant.order(2,0)// destructuring
+console.log(food1, food2)
+
+// Nested destructuring
+const nested = [2, 4, [5, 6]]
+const [val1, ,val2] = nested
+console.log(val1, val2) // taking values 2 and array out
+
+const [i, , [j, k]] = nested // taking values 2, and 5,6 (ie values inside array) out // destructuring inside of destructuring to get values of nested array
+console.log(i,j,k)
+
+// Default Values // setting default values for variables when we are extracting them . Useful in case if we don't know the actual length of array
+const [p=1, q=1, r=1] = [8,9]
+console.log(p, q, r) // If no default values are given then r returns undefined
